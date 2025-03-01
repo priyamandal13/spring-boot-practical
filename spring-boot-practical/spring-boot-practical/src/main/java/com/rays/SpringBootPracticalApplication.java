bd68e1c44f4973bd1c3fa35a253cadde058d622e
priyamandal13 <apriyamandal13@gmail.com>
2025-03-01 16:45:47 +0530

    done

---
+++ spring-boot-practical/spring-boot-practical/src/main/java/com/rays/SpringBootPracticalApplication.java
@@ -0,0 +1,14 @@
+package com.rays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootPracticalApplication.class, args);
+
+	}
+}
